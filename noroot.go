7ceabc7517d58b4232958d38df0f6f6bad5148ea
Jonathan Cooper <cooper@cooperthecoder.com>
2024-08-02 09:45:23 +0100

    Installed docker and docker-compose

--- noroot.go
+++ noroot.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
-    "fmt"
-    "log"
+	"log"
 	"os"
 	"regexp"
 	"strings"
@@ -22,13 +22,22 @@ packages:
   - ntp
   - unattended-upgrades
   - auditd
+  - apt-transport-https
+  - ca-certificates
+  - curl
+  - software-properties-common
 
 users:
-  - name: {{.Username}}
+  - name: {{.AdminUsername}}
     sudo: ['ALL=(ALL) NOPASSWD:ALL']
     shell: /bin/bash
     ssh-authorized-keys:
-      - {{.SSHKey}}
+      - {{.AdminSSHKey}}
+  - name: {{.DockerUsername}}
+    groups: docker
+    shell: /bin/bash
+    ssh_pwauth: false
+    lock_passwd: true
 
 ssh_pwauth: false
 
@@ -36,10 +45,15 @@ disable_root: true
 
 chpasswd:
   list: |
-     {{.Username}}:password
+     {{.AdminUsername}}:password
   expire: false
 
 runcmd:
+  - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  - add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  - apt-get update
+  - apt-get install -y docker-ce docker-compose
+  - gpasswd -a {{.DockerUsername}} docker
   - systemctl enable fail2ban
   - systemctl start fail2ban
   - systemctl enable ntp
@@ -47,41 +61,53 @@ runcmd:
   - dpkg-reconfigure -plow unattended-upgrades
   - systemctl enable auditd
   - systemctl start auditd
-  - mkdir -p /home/{{.Username}}/.ssh
-  - echo '{{.SSHKey}}' > /home/{{.Username}}/.ssh/authorized_keys
-  - chown -R {{.Username}}:{{.Username}} /home/{{.Username}}/.ssh
-  - chmod 600 /home/{{.Username}}/.ssh/authorized_keys
+  - mkdir -p /home/{{.AdminUsername}}/.ssh
+  - echo '{{.AdminSSHKey}}' > /home/{{.AdminUsername}}/.ssh/authorized_keys
+  - chown -R {{.AdminUsername}}:{{.AdminUsername}} /home/{{.AdminUsername}}/.ssh
+  - chmod 600 /home/{{.AdminUsername}}/.ssh/authorized_keys
+  - mkdir -p /home/{{.DockerUsername}}/.ssh
+  - chown -R {{.DockerUsername}}:{{.DockerUsername}} /home/{{.DockerUsername}}/.ssh
+  - chmod 700 /home/{{.DockerUsername}}/.ssh
+  - touch /home/{{.DockerUsername}}/.hushlogin
+  - systemctl enable docker
+  - reboot
 
 final_message: "The system is finally up, after $UPTIME seconds"
 `
 
 type CloudInitData struct {
-	Username string
-	SSHKey   string
+	AdminUsername  string
+	AdminSSHKey    string
+	DockerUsername string
 }
 
 func main() {
 	// Parse command line arguments
-	keyFile := flag.String("keyfile", os.Getenv("HOME")+"/.ssh/id_rsa.pub", "Path to the SSH public key file")
+	adminKeyFile := flag.String("adminkeyfile", os.Getenv("HOME")+"/.ssh/id_rsa.pub", "Path to the admin SSH public key file")
 	outputFile := flag.String("output", "cloud-init.yml", "Path to the output YAML file")
-	username := flag.String("username", "topman", "Username for the new user")
+	adminUsername := flag.String("adminusername", "topman", "Username for the admin user")
+	dockerUsername := flag.String("dockerusername", "dockeruser", "Username for the docker user")
 	flag.Parse()
 
-	// Validate the username
-	if !isValidUsername(*username) {
-		log.Fatalf("Invalid username: %s. Must be 1-32 characters long and contain only lowercase letters, numbers, and underscores.\n", *username)
+	// Validate the usernames
+	if !isValidUsername(*adminUsername) {
+		log.Fatalf("Invalid admin username: %s. Must be 1-32 characters long and contain only lowercase letters, numbers, and underscores.\n", *adminUsername)
+	}
+	if !isValidUsername(*dockerUsername) {
+		log.Fatalf("Invalid docker username: %s. Must be 1-32 characters long and contain only lowercase letters, numbers, and underscores.\n", *dockerUsername)
 	}
 
-	// Read and validate the SSH key from the specified file
-	key, err := readSSHKey(*keyFile)
+	// Read and validate the SSH keys from the specified files
+	adminKey, err := readSSHKey(*adminKeyFile)
 	if err != nil {
-		log.Fatalf("Error reading SSH key: %v\n", err)
+		log.Fatalf("Error reading admin SSH key: %v\n", err)
 	}
 
 	// Prepare data for the template
 	data := CloudInitData{
-		Username: *username,
-		SSHKey:   key,
+		AdminUsername:  *adminUsername,
+		AdminSSHKey:    adminKey,
+		DockerUsername: *dockerUsername,
 	}
 
 	// Generate the cloud-init YAML content using the template
@@ -96,8 +122,10 @@ func main() {
 	}
 
 	// Validate the YAML content
-	if !isValidYAML(yamlContent.String()) {
-		log.Fatalf("Generated YAML content is invalid\n")
+	var out map[string]interface{}
+	err = yaml.Unmarshal([]byte(yamlContent.String()), &out)
+	if err != nil {
+		log.Printf("Generated YAML content is invalid: %v\n", err)
 	}
 
 	// Write the YAML content to the output file
@@ -110,31 +138,24 @@ func main() {
 
 // readSSHKey reads the SSH key from the given file path and validates its structure
 func readSSHKey(filePath string) (string, error) {
-	file, err := os.Open(filePath)
+	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		return "", err
 	}
-	defer func(file *os.File) {
-        err := file.Close()
-        if err != nil {
-            log.Fatalf("Error closing SSH key: %v", err)
-        }
-    }(file)
+	key := getFirstLine(string(fileContent))
+	if !isValidSSHKey(key) {
+		return "", errors.New("invalid SSH key format")
+	}
+	return key, nil
+}
 
-	scanner := bufio.NewScanner(file)
+// getFirstLine returns the first line of the given string.
+func getFirstLine(input string) string {
+	scanner := bufio.NewScanner(strings.NewReader(input))
 	if scanner.Scan() {
-		key := scanner.Text()
-		_, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
-		if err != nil {
-			return "", fmt.Errorf("invalid SSH key format: %v", err)
-		}
-		return key, nil
+		return scanner.Text()
 	}
-	if err := scanner.Err(); err != nil {
-		return "", err
-	}
-
-	return "", fmt.Errorf("no SSH key found in file %s", filePath)
+	return ""
 }
 
 // isValidUsername validates the username
@@ -143,16 +164,8 @@ func isValidUsername(username string) bool {
 	return validUsername.MatchString(username)
 }
 
-// isValidYAML validates the YAML content
-func isValidYAML(content string) bool {
-	var out map[string]interface{}
-	return getFirstLine(content) == "#cloud-config" && yaml.Unmarshal([]byte(content), &out) == nil
-}
-
-func getFirstLine(input string) string {
-	scanner := bufio.NewScanner(strings.NewReader(input))
-	if scanner.Scan() {
-		return scanner.Text()
-	}
-	return ""
+// isValidSSHKey validates the SSH key
+func isValidSSHKey(key string) bool {
+	_, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
+	return err == nil
 }
